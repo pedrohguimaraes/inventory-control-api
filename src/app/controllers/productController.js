58665e5147dc39d9da20cbb04dc089535cb2b241
Pedro Guimarães <phgs1996@gmail.com>
2019-03-19 08:51:58 -0300

    start archives

---
+++ src/app/controllers/productController.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {Usuario} = require('../models');
+const authConfig = require('../../resources/auth/authConfig');
+const mailer = require('../../modules/mailer');
+
+// list
+router.post('/', async(req, res) => {
+
+    try{
+        const postData = req.body;
+
+       return res.status(200).send(retorno);
+    }catch(err){
+        
+        return res.status(400).send(err);
+    }
+});
+
+// store
+router.post('/store', async(req, res) => {
+
+    try{
+        const postData = req.body;
+
+        return res.status(400).send(retorno);
+    }catch(err){
+        return res.status(201).send(err);
+    }
+});
+
+module.exports = app => app.use('/auth', router);  
