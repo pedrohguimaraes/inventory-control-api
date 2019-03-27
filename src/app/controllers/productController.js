c11924e4d00c0bdc02261d9db3aa9a51e81fef79
Pedro Guimarães <phgs1996@gmail.com>
2019-03-27 12:37:46 -0300

    source

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const {Product} = require('../models');
+//const {Product} = require('../models');
 
 
 // list
